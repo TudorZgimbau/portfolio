551674540c05b2b59810d7b370a0f19dc3a00c90
Zgîmbău Tudor <tudor.zgimbau@gmail.com>
2023-05-31 19:45:37 +0300

    Revert to SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   adapter: vercel({
     analytics: true,
   }),
