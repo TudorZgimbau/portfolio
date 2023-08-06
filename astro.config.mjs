6f59fa378ec8f9009030dc7ce18dda8d8b2d1b2a
Zgîmbău Tudor <tudor.zgimbau@gmail.com>
2023-08-06 16:59:50 +0300

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel({
     analytics: true,
   }),
-  integrations: [tailwind()],
+  site: "https://www.tudor-zgimbau.dev",
+  integrations: [tailwind(), sitemap()],
 });
