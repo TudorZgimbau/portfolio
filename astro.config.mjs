7aa7dc8236b6656ce837fee28570695edf5b222e
Zgîmbău Tudor <tudor.zgimbau@gmail.com>
2024-02-16 14:30:53 +0200

    Remove unnecessary Vercel adapter
    Only needed for SSR or hybrid builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    webAnalytics: true,
-  }),
   site: "https://www.tudor-zgimbau.dev",
   integrations: [tailwind(), sitemap()],
 });
