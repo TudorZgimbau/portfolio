80aee53abad3d9de2d31d45890b9b7e2d10d9d37
Zgîmbău Tudor <tudor.zgimbau@gmail.com>
2024-02-16 14:39:32 +0200

    Misread the docs, add the adapter again and fix the configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   site: "https://www.tudor-zgimbau.dev",
   integrations: [tailwind(), sitemap()],
 });
